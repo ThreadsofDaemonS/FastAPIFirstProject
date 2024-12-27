3d9909192049fa72ad6bfd0f4fcfd2433fb3b1b0
W Q <vbnzuf@gmail.com>
2024-12-27 13:32:55 +0200

    Users added

--- main.py
+++ main.py
@@ -4,39 +4,43 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-async def root() -> dict[str, str]:
-    return {"message": "Hello, World!"}
-
-@app.get("/contacts")
-async def get_contacts() -> list[dict[str, str]]:
-    contacts = [
-        {"id": 1, "name": "Alice", "email": "alice@example.com"},
-        {"id": 2, "name": "Bob", "email": "bob@example.com"},
-        {"id": 3, "name": "Charlie", "email": "charlie@example.com"}
-    ]
-    return contacts
-
-@app.get('/items')
-async def read_items() -> list[dict]:
-    return posts
-
 
 from pydantic import BaseModel
+
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+users = [
+    {"id": 1, "name": "John Doe", "age": 30},
+    {"id": 2, "name": "Cat", "age": 25},
+    {"id": 3, "name": "Dog", "age": 54},
+]
+
 
 class Post(BaseModel):
     id: int
     title: str
     content: str
+    author: User
 
 posts = [
-    Post(id=1, title="First Post", content="This is First Post"),
-    Post(id=2, title="Second Post", content="This is Second Post"),
-    Post(id=3, title="Third Post", content="This is Third Post")
+    Post(id=1, title="First Post", content="This is First Post", author=users[0] ),
+    Post(id=2, title="Second Post", content="This is Second Post", author=users[1] ),
+    Post(id=3, title="Third Post", content="This is Third Post", author=users[2] )
 ]
 
+
+
+@app.get('/items')
+async def read_items() -> list[Post]:
+    return posts
+
 @app.get("/items/{item_id}", response_model=Post)
-async def read_item(item_id: int):
+async def read_item(item_id: int) -> Post:
     for post in posts:
         if post.id == item_id:
             return post
@@ -44,14 +48,14 @@ async def read_item(item_id: int):
     raise HTTPException(status_code=404, detail="Post not found")
 
 @app.get("/search")
-async def search(post_id: int | None = None):
+async def search(post_id: int | None = None) -> Post | dict:
     if post_id:
         for post in posts:
             if post.id == post_id:
                 return post
         raise HTTPException(status_code=404, detail="Post not found")
     else:
-        return {"error": "No post_id provided"}
+        return {"error": None}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
