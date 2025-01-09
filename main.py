2f8c984efbbb6863f8bc818f41d551bc881b98a3
W Q <vbnzuf@gmail.com>
2025-01-09 09:54:54 +0200

    Add db and models, also refactor the code added schemas

--- main.py
+++ main.py
@@ -1,101 +1,99 @@
-from fastapi import FastAPI, HTTPException, Path, Query, Body
+from fastapi import FastAPI, HTTPException, Path, Query, Body, Depends
 from pydantic import BaseModel
 import uvicorn
-from pydantic import BaseModel, Field
 from typing import Annotated
+from sqlalchemy.orm import Session
+
+from models import User, Post, Base
+from database import session_local, engine
+from schemas import PostCreate, UserCreate, PostResponse, User as DbUser
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    try:
+        db = session_local()
+        yield db
+    finally:
+        db.close()
+
+@app.post("/users/", response_model=DbUser)
+async def create_user(user: UserCreate, db: Session = Depends(get_db)) -> DbUser:
+    db_user = User(name=user.name, age=user.age)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
 
 
-
-class PostCreate(BaseModel):
-    title: str
-    body: str
-    author_id: int
-
-class User(BaseModel):
-    id: int
-    name: str
-    age: int
-
-class Post(BaseModel):
-    id: int
-    title: str
-    body: str
-    author: User
-
-class UserCreate(BaseModel):
-    name: Annotated[
-        str, Field(..., title="Name of user", min_length=3, max_length=50)
-    ]
-    age: Annotated[
-        int, Field(..., title="Age of user", ge=2, le=120)
-    ]
-
-users = [
-    {"id": 1, "name": "John Doe", "age": 30},
-    {"id": 2, "name": "Mike", "age": 25},
-    {"id": 3, "name": "Alice", "age": 44}
-]
-
-
-
-posts = [
-    {"id": 1, "title": "Foo", "body": "A very long description", "author": users[0]},
-    {"id": 2, "title": "Foo", "body": "A very long description", "author": users[1]},
-    {"id": 3, "title": "Foo", "body": "A very long description", "author": users[2]}
-]
-
-
-
-@app.get("/items")
-async def get_items() -> list[Post]:
-    return [Post(**post) for post in posts]
-
-@app.post("/items/add")
-async def add_item(post: PostCreate) -> Post:
-    author = next((user for user in users if user['id'] == post.author_id), None)
-    if not author:
+@app.post("/posts/", response_model=PostResponse)
+async def create_post(post: PostCreate, db: Session = Depends(get_db)) -> PostResponse:
+    db_user = db.query(User).filter(User.id == post.author_id).first()
+    if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
 
-    new_post_id = len(posts) + 1
-    new_post = {"id": new_post_id, "title": post.title, "body": post.body, "author": author}
-    posts.append(new_post)
+    db_post = Post(title=post.title, body=post.body, author_id=post.author_id)
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
 
-    return Post(**new_post)
+    return db_post
 
-@app.post("/user/add")
-async def add_user(user: Annotated[UserCreate, Body(..., example={
-    "name": "John Doe",
-    "age": 30
-})]) -> User:
-    new_user_id = len(users) + 1
-    new_user = {"id": new_user_id, "name": user.name, "age": user.age}
-    users.append(new_user)
-
-    return User(**new_user)
+@app.get("/posts/", response_model=list[PostResponse])
+async def get_posts(db: Session = Depends(get_db)) -> list[PostResponse]:
+    return db.query(Post).all()
 
 
-@app.get("/items/{item_id}")
-async def get_item(item_id: Annotated[int, Path(..., title='The post ID is specified here', ge=1, lt=100)]) -> Post:
-    for post in posts:
-        if post["id"] == item_id:
-            return Post(**post)
-
-    raise HTTPException(status_code=404, detail="Item not found")
-
-@app.get("/search")
-async def search(post_id: Annotated[
-    int | None, Query(title="ID of post to search", ge=1, lt=100)
-]) -> Post | dict:
-    if post_id:
-        for post in posts:
-            if post["id"] == post_id:
-                return {"data": Post(**post)}
-        raise HTTPException(status_code=404, detail="Post not found")
-    else:
-        return {"error": None}
+# @app.get("/items")
+# async def get_items() -> list[Post]:
+#     return [Post(**post) for post in posts]
+#
+# @app.post("/items/add")
+# async def add_item(post: PostCreate) -> Post:
+#     author = next((user for user in users if user['id'] == post.author_id), None)
+#     if not author:
+#         raise HTTPException(status_code=404, detail="User not found")
+#
+#     new_post_id = len(posts) + 1
+#     new_post = {"id": new_post_id, "title": post.title, "body": post.body, "author": author}
+#     posts.append(new_post)
+#
+#     return Post(**new_post)
+#
+# @app.post("/user/add")
+# async def add_user(user: Annotated[UserCreate, Body(..., example={
+#     "name": "John Doe",
+#     "age": 30
+# })]) -> User:
+#     new_user_id = len(users) + 1
+#     new_user = {"id": new_user_id, "name": user.name, "age": user.age}
+#     users.append(new_user)
+#
+#     return User(**new_user)
+#
+#
+# @app.get("/items/{item_id}")
+# async def get_item(item_id: Annotated[int, Path(..., title='The post ID is specified here', ge=1, lt=100)]) -> Post:
+#     for post in posts:
+#         if post["id"] == item_id:
+#             return Post(**post)
+#
+#     raise HTTPException(status_code=404, detail="Item not found")
+#
+# @app.get("/search")
+# async def search(post_id: Annotated[
+#     int | None, Query(title="ID of post to search", ge=1, lt=100)
+# ]) -> Post | dict:
+#     if post_id:
+#         for post in posts:
+#             if post["id"] == post_id:
+#                 return {"data": Post(**post)}
+#         raise HTTPException(status_code=404, detail="Post not found")
+#     else:
+#         return {"error": None}
 
 
 
---
+++ schemas.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, Field
+
+
+class UserBase(BaseModel):
+    name: str
+    age: int
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class UserCreate(UserBase):
+    pass
+
+
+class PostBase(BaseModel):
+    title: str
+    body: str
+    author_id: int
+
+class PostCreate(PostBase):
+    pass
+
+class PostResponse(PostBase):
+    id: int
+    author: User
+
+
+    class Config:
+        orm_mode = True
+
