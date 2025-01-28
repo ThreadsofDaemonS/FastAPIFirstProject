6f0ac82bcbf4cde3477daa80a1c46141bc5b2291
W Q <vbnzuf@gmail.com>
2025-01-28 15:29:13 +0200

    Made models asynchronius

--- models.py
+++ models.py
@@ -1,22 +1,24 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs
 from database import Base
-from sqlalchemy.orm import relationship
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    age = Column(Integer)
 
 
+class User(Base, AsyncAttrs):  # Подключаем асинхронное поведение
+    __tablename__ = "users"
 
-class Post(Base):
-    __tablename__ = 'posts'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String, index=True)
+    age: Mapped[int] = mapped_column(Integer)
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    body = Column(String)
-    author_id = Column(Integer, ForeignKey('users.id'))
 
-    author = relationship("User")
+class Post(Base, AsyncAttrs):  # Подключаем асинхронное поведение
+    __tablename__ = "posts"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String, index=True)
+    body: Mapped[str] = mapped_column(String)
+    author_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+
+    # Указываем связь с User
+    author: Mapped[User] = relationship("User", lazy="joined")  # Используем `joined` для предзагрузки
